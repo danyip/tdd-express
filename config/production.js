88aba5a2e04f4b128f17d5968114a0ab834b9e23
danyip <hello@danyip.com>
2022-05-12 19:19:37 +1000

    clean up scripts and add prod script

---
+++ config/production.js
@@ -0,0 +1,20 @@
+module.exports = {
+  database: {
+    database: 'hoaxify',
+    username: 'my-db-user',
+    password: 'db-p4ss',
+    dialect: 'sqlite',
+    storage: './prod-db.sqlite',
+    logging: false,
+  },
+  mail: {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'vlhf7l5b4teclhpo@ethereal.email',
+      pass: 'yakKpWYnDyBqUJAXeh',
+    },
+  },
+  uploadDir: 'uploads-production',
+  profileDir: 'profile',
+};
